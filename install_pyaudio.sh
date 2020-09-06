49656f751917412fd0c4a7d5dc2bdf9e006c0612
Abhishek C. Salian <43337429+AbhishekSalian@users.noreply.github.com>
2020-09-06 22:16:52 +0530

    # Adding script file for installation

---
+++ install_pyaudio.sh
@@ -0,0 +1,11 @@
+# credits for solution: https://stackoverflow.com/a/35593426/14055695
+
+sudo apt-get install libasound-dev
+echo "Downloading the headerfile........"
+sudo curl http://portaudio.com/archives/pa_stable_v190600_20161030.tgz | sudo tar -xz &&
+echo "Configuring ....."
+cd portaudio && ./configure && make && sudo make install &&
+echo "Installing pyaudio .... " &&
+sudo pip install pyaudio &&
+echo "Installation Completed---------"
+

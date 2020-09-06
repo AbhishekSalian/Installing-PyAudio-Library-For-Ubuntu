289ace7cc4c555dbc344ae82261b094399ea720b
Abhishek C. Salian <43337429+AbhishekSalian@users.noreply.github.com>
2020-09-06 22:24:21 +0530

    # correcting path
    cd pyaudio -> cd pyaudio/

--- install_pyaudio.sh
+++ install_pyaudio.sh
@@ -4,7 +4,7 @@ sudo apt-get install libasound-dev
 echo "Downloading the headerfile........"
 sudo curl http://portaudio.com/archives/pa_stable_v190600_20161030.tgz | sudo tar -xz &&
 echo "Configuring ....."
-cd portaudio && ./configure && make && sudo make install &&
+cd portaudio/ && ./configure && make && sudo make install &&
 echo "Installing pyaudio .... " &&
 sudo pip install pyaudio &&
 echo "Installation Completed---------"
